23cc2c4934860a0075a08e8db9c337a012ca7882
KamilUr <kam.urbanski@gmail.com>
2018-03-22 20:25:28 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+@EnableAutoConfiguration
+public class MainController {
+
+    @GetMapping("/")
+@ResponseBody
+    public  String home(){
+return "Hello world";
+
+    }
+
+
+}
